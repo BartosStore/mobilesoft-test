74472ab3eade0b6b0ade133d1fed4a21392567f1
BartosStore <mirek-b@seznam.cz>
2020-03-21 18:41:30 +0100

    Style form

--- lib/form/form_page.dart
+++ lib/form/form_page.dart
@@ -12,15 +12,48 @@ class _FormPageState extends State<FormPage> {
       appBar: AppBar(
         title: Text('New employee'),
       ),
-      body: Form(
-          child: Column(
-        children: <Widget>[
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-          TextFormField(),
-        ],
-      )),
+      body: Container(
+        padding: EdgeInsets.all(16.00),
+        child: Form(
+            child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: CircleAvatar(
+                    radius: 64.0,
+                    backgroundImage:
+                        AssetImage('assets/images/person_placeholder.png'),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Name'),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Address'),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Address'),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Address'),
+            ),
+            RaisedButton(
+              child: Text('Save'),
+              onPressed: () {},
+            ),
+          ],
+        )),
+      ),
     );
   }
 }
