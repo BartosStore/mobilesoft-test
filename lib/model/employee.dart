b26b1fd731f711250b528d09b9ba4c51556c95a4
BartosStore <mirek-b@seznam.cz>
2020-03-21 16:14:31 +0100

    Create model class Employee

---
+++ lib/model/employee.dart
@@ -0,0 +1,11 @@
+class Employee {
+  Employee({this.name, this.department, this.address, this.salary, this.photo});
+
+  final String name;
+  final String department;
+  final String address;
+  final double salary;
+  final String photo;
+
+  // todo: serialization
+}
