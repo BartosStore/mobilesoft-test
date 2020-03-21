63976922937afa55a66632b5ee38a5605575af3a
BartosStore <mirek-b@seznam.cz>
2020-03-21 16:25:26 +0100

    Generate full Employee record

--- lib/common/generator.dart
+++ lib/common/generator.dart
@@ -1,5 +1,13 @@
 import 'package:mobilesoft/model/employee.dart';
 
 List<Employee> generateListOfNEmployees(int count) {
-  return List.generate(count, (index) => Employee(name: 'Josef Novak $index'));
+  return List.generate(
+    count,
+    (index) => Employee(
+      name: 'Josef Novak $index',
+      department: 'Sales department',
+      address: 'Hradec Kralove',
+      salary: 28000.00,
+    ),
+  );
 }
