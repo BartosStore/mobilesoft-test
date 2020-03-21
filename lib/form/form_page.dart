b49d3fb31f546e75c94c727884debc5d82d54717
BartosStore <mirek-b@seznam.cz>
2020-03-21 18:42:10 +0100

    Add form validation

--- lib/form/form_page.dart
+++ lib/form/form_page.dart
@@ -6,53 +6,71 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final Function _validator = (String value) {
+    if (value.isEmpty) return 'Cannot by empty';
+    return null;
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('New employee'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.00),
-        child: Form(
-            child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: CircleAvatar(
-                    radius: 64.0,
-                    backgroundImage:
-                        AssetImage('assets/images/person_placeholder.png'),
-                  ),
-                ),
-                Expanded(
-                  child: Column(
+      body: Builder(
+        builder: (BuildContext ctx) => Container(
+          padding: EdgeInsets.all(16.00),
+          child: Form(
+              key: _formKey,
+              child: ListView(
+                children: <Widget>[
+                  Row(
                     children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Name'),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: CircleAvatar(
+                          radius: 64.0,
+                          backgroundImage: AssetImage(
+                              'assets/images/person_placeholder.png'),
+                        ),
                       ),
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Address'),
-                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(labelText: 'Name'),
+                              validator: _validator,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(labelText: 'Address'),
+                              validator: _validator,
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
-                )
-              ],
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Address'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Address'),
-            ),
-            RaisedButton(
-              child: Text('Save'),
-              onPressed: () {},
-            ),
-          ],
-        )),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Department'),
+                    validator: _validator,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Salary'),
+                    validator: _validator,
+                  ),
+                  RaisedButton(
+                    child: Text('Save'),
+                    onPressed: () {
+                      _formKey.currentState.validate();
+                      Scaffold.of(ctx).showSnackBar(
+                          SnackBar(content: Text('Not implemented yet')));
+                    },
+                  ),
+                ],
+              )),
+        ),
       ),
     );
   }
