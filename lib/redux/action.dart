e31e1c583a1b3eb557dcc8c22ac2926d66e99565
BartosStore <mirek-b@seznam.cz>
2020-03-22 16:59:37 +0100

    List employees from database

--- lib/redux/action.dart
+++ lib/redux/action.dart
@@ -1,6 +1,7 @@
 import 'package:redux/redux.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
+import 'package:mobilesoft/api/database_client.dart';
 import 'package:mobilesoft/api/api_client.dart';
 import 'package:mobilesoft/model/employee.dart';
 import 'package:mobilesoft/redux/app_state.dart';
@@ -15,6 +16,18 @@ ThunkAction<AppState> fetchEmployees() {
   return (Store<AppState> store) async {
     // mock api call
     ApiClient().fetchEmployees().then((employees) {
+      // save to DB
+      for (var e in employees) {
+        DatabaseClient.insert(Employee.table, e);
+      }
+      // store.dispatch(AddAllEmployeesAction(employees: employees));
+    });
+
+    // get from DB
+    // propagate items to store
+    DatabaseClient.query(Employee.table).then((employeesMap) {
+      var employees =
+          employeesMap.map((item) => Employee.fromMap(item)).toList();
       store.dispatch(AddAllEmployeesAction(employees: employees));
     });
   };
